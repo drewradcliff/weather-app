4aea69608311d9cff02d5c12d3fd6fd6afd2447f
Drew Radcliff <sirampr@gmail.com>
2020-11-21 12:40:05 -0500

    Get weather data from API

--- src/api/weather-api.js
+++ src/api/weather-api.js
@@ -5,16 +5,14 @@ const BASE_URL = "https://api.weatherapi.com/v1";
 
 // API DOCUMENTATION: https://www.weatherapi.com/docs/#
 
-export { getCurrentWeather, getForecast };
-
-const getCurrentWeather = (queryLocation) => {
+export const getCurrentWeather = (queryLocation) => {
   const url = `${BASE_URL}/current.json?key=${KEY}&q=${queryLocation}`;
-
+  
   return axios.get(url).then((response) => response.data);
 };
 
-const getForecast = (queryLocation, queryDays) => {
+export const getForecast = (queryLocation, queryDays) => {
   const url = `${BASE_URL}/forecast.json?key=${KEY}&q=${queryLocation}&days=${queryDays}`;
-
+  
   return axios.get(url).then((response) => response.data);
 };
